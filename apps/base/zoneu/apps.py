ac767f28432c2e8d1cde0e3db245a1ebbb1ec7fc
mypc-workstation <2689179206@qq.com>
2019-10-18 22:03:56 +0800

    ultratool v5.0.0.3

---
+++ apps/base/zoneu/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ZoneuConfig(AppConfig):
+    name = 'zoneu'
---
+++ apps/ext/workorder3pl/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Workorder3PlConfig(AppConfig):
+    name = 'workorder3pl'
